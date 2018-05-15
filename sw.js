050ba9be885dc3faaba494c168e8cb0c4d03cd6c
johnrieth <john.rieth@outlook.com>
2018-05-14 18:09:34 -0700

    ahhh

--- sw.js
+++ sw.js
@@ -6,9 +6,20 @@ self.addEventListener('install', function(e) {
         'index.html',
         'uswds.min.js',
         'styleguide.css',
-        'assets/fonts/',
         'assets/img/favicons/favicon-57.png',
-        'assets/img/hero.png'
+        'assets/img/hero.png',
+        'assets/img/close.svg',
+        'assets/img/icon-htts.svg',
+        'assets/img/icon-dot-gov.svg',
+        'assets/img/circle-124.svg',
+        'assets/img/logo-img.png',
+        'assets/img/social-icons/svg/youtube15.svg',
+        'assets/img/social-icons/svg/twitter16.svg',
+        'assets/img/social-icons/svg/facebook25.svg',
+        'assets/fonts/sourcesanspro-bold-webfont.ttf',
+        'assets/fonts/merriweather-regular-webfont.ttf',
+        
+
 
       ]);
     })
