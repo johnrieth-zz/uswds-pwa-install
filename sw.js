b7fe7a940d7d6a1be6d7e2c69926cb0b98ea5f15
johnrieth <john.rieth@outlook.com>
2018-05-14 17:35:50 -0700

    hello

--- sw.js
+++ sw.js
@@ -1,21 +1,23 @@
 self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open('video-store').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/index.js',
-          '/styleguide.css'
-        ]);
-      })
-    );
-   });
-   
-   self.addEventListener('fetch', function(e) {
-     console.log(e.request.url);
-     e.respondWith(
-       caches.match(e.request).then(function(response) {
-         return response || fetch(e.request);
-       })
-     );
-   });
+  e.waitUntil(
+    caches.open('video-store').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'uswds.min.js',
+        'styleguide.css',
+        'assets/fonts/',
+        'assets/img/'
+      ]);
+    })
+  );
+ });
+ 
+ self.addEventListener('fetch', function(e) {
+   console.log(e.request.url);
+   e.respondWith(
+     caches.match(e.request).then(function(response) {
+       return response || fetch(e.request);
+     })
+   );
+ });
